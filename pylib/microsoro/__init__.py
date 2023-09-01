ef9a464dbba98765ce9db43af39219b21ee79338
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-08-31 20:16:40 -0400

    Implement, test microsoro apply_rotate

--- pylib/microsoro/__init__.py
+++ pylib/microsoro/__init__.py
@@ -1,6 +1,8 @@
+from .apply_rotate import apply_rotate
 from .State import State
 
 
 __all__ = [
+    "apply_rotate",
     "State",
 ]
---
+++ pylib/test/test_microsoro/test_apply_rotate.py
@@ -0,0 +1,37 @@
+import copy
+
+import numpy as np
+import pytest
+
+from pylib.microsoro import apply_rotate, State
+
+
+@pytest.mark.parametrize(
+    "rotate_angle", [-180, -90, -45, 33, 45, 90, 180, 360]
+)
+def test_wraparound(rotate_angle: int):
+    state = State(height=8, width=10)
+    state_ = copy.deepcopy(state)
+
+    cur_angle = 0
+    for __ in range(20):
+        apply_rotate(state, rotate_angle)
+        cur_angle += rotate_angle
+        cur_angle %= 360
+        for mata, matb in [(state.px, state_.px), (state.py, state_.py)]:
+            assert (cur_angle == 0) == np.isclose(mata, matb).all()
+
+
+def test_pairwise_distance():
+    state = State(height=8, width=10)
+
+    distances = []
+    for i in range(20):
+        apply_rotate(state, i)
+        pxa, *__, pxb = state.px.flat
+        pya, *__, pyb = state.py.flat
+
+        distance = (pxa - pxb) ** 2 + (pya - pyb) ** 2
+        distances.append(distance)
+
+    assert np.isclose(distances[:-1], distances[1:]).all()
