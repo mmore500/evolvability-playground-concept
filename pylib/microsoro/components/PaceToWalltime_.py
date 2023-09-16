e2de1014c4ebd13dcf4136983a32a22718437353
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-09-16 17:39:29 -0400

    Bugfix: respect time dilation in walltime pacer

--- pylib/microsoro/components/PaceToWalltime_.py
+++ pylib/microsoro/components/PaceToWalltime_.py
@@ -5,6 +5,7 @@ import pause
 
 from ..Params import Params
 from ..State import State
+from ..Style import Style
 
 
 class PaceToWalltime:
@@ -12,11 +13,13 @@ class PaceToWalltime:
 
     _allow_catchup: bool
     _params: Params
+    _style: Style
     _until_time: typing.Optional[datetime.datetime]
 
     def __init__(
         self: "RecordVideoPyglet",
         params: typing.Optional[Params] = None,
+        style: typing.Optional[Style] = None,
         allow_catchup: bool = False,
     ) -> None:
         """Initialize functor.
@@ -27,6 +30,10 @@ class PaceToWalltime:
             What is the simulation time delta between update steps?
 
             Defaults to default-initialized Params.
+        style : Style, optional
+            Simulation should proceed at what ratio of real time?
+
+            Defaults to default-initialized Style.
         allow_catchup : bool, default False
             Should faster-than-realtime updates be allowed if simulation has
             fallen behind realtime pace?
@@ -34,6 +41,10 @@ class PaceToWalltime:
         if params is None:
             params = Params()
         self._params = params
+
+        if style is None:
+            style = Style()
+        self._style = style
 
         self._allow_catchup = allow_catchup
         self._until_time = None
@@ -48,4 +59,5 @@ class PaceToWalltime:
         now = datetime.datetime.now
         from_time = self._until_time if self._allow_catchup else now()
         dt = datetime.timedelta(seconds=self._params.dt)
-        self._until_time = from_time + dt
+        time_dilation = self._style.time_dilation
+        self._until_time = from_time + dt * time_dilation
