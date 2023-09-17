b964b8c81206bfefcdd46157ff73632ba02a20fb
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-09-17 15:29:22 -0400

    Add idempotify option to decorate_with_context

--- pylib/auxlib/decorate_with_context.py
+++ pylib/auxlib/decorate_with_context.py
@@ -1,9 +1,16 @@
+import contextlib
 import functools
+import threading
 import typing
 
 
 def decorate_with_context(
     context_manager_factory: typing.Callable,
+    idempotify_decorated_context: bool = False,
+    *,
+    # active context manager factory ids
+    # global state shared across all calls to decorate_with_context
+    _active_ids: typing.Set[int] = set(),
 ) -> typing.Callable:
     """Decorate a function to run within an instance of the given context
     manager.
@@ -12,6 +19,10 @@ def decorate_with_context(
     ----------
     context_manager_factory : Callable[[...], ContextManager]
         The context manager with which to decorate the function.
+
+    idempotify_decorated_context: bool, default False
+        Should invocations of context_manager_factory be tracked and only one
+        live invocation allowed at any one time?
 
     Returns
     -------
@@ -29,10 +40,33 @@ def decorate_with_context(
     Context manager constructor arguments are not supported.
     """
 
+    cmf_id = id(context_manager_factory)
+
     def decorator(func: typing.Callable) -> typing.Callable:
         @functools.wraps(func)
         def wrapper(*args, **kwargs) -> typing.Any:
-            with context_manager_factory():
+            context: typing.Callable
+            # no idempotency concerns
+            if not idempotify_decorated_context:
+                context = context_manager_factory
+            # if idempotency enforced and factory is live, do no-op
+            elif cmf_id in _active_ids:
+                assert idempotify_decorated_context
+                context = contextlib.nullcontext
+            # if idempotency enforced and factory is not live, mark live
+            else:
+                assert idempotify_decorated_context
+
+                @contextlib.contextmanager
+                def context() -> typing.Iterable:
+                    with context_manager_factory():
+                        try:
+                            _active_ids.add(cmf_id)
+                            yield
+                        finally:
+                            _active_ids.discard(cmf_id)
+
+            with context():
                 return func(*args, **kwargs)
 
         return wrapper
--- pylib/test/test_auxlib/test_decorate_with_context.py
+++ pylib/test/test_auxlib/test_decorate_with_context.py
@@ -1,25 +1,67 @@
-from contextlib import contextmanager
+import contextlib
+import typing
+
+import pytest
 
 from pylib.auxlib import decorate_with_context
 
 
-@contextmanager
-def dummy_context():
-    global context_entered
-    context_entered = True
+@contextlib.contextmanager
+def dummy_context(state: typing.Dict) -> typing.Iterable:
+    state["context_active"] = True
+    state["invocation_count"] += 1
     yield
-    context_entered = False
+    state["context_active"] = False
 
 
-def test_decorate_with_context():
-    global context_entered
+@pytest.fixture
+def state():
+    return {"context_active": False, "invocation_count": 0}
 
-    context_entered = False
 
-    @decorate_with_context(dummy_context)
+def test_without_idempotency(state: typing.Dict):
+    @decorate_with_context(lambda: dummy_context(state))
     def sample_function():
-        assert context_entered == True
+        assert state["context_active"] == True
 
     sample_function()
+    assert state["context_active"] == False
+    assert state["invocation_count"] == 1
 
-    assert context_entered == False
+
+def test_with_idempotency_without_nested_call(state: typing.Dict):
+    @decorate_with_context(
+        lambda: dummy_context(state), idempotify_decorated_context=True
+    )
+    def sample_function_idempotent():
+        assert state["context_active"] == True
+
+    sample_function_idempotent()
+    assert state["context_active"] == False
+    assert state["invocation_count"] == 1
+
+
+@pytest.mark.parametrize("idempotify", [True, False])
+def test_with_idempotency_with_nested_call(
+    state: typing.Dict, idempotify: bool
+):
+    dummy_context_factory = lambda: dummy_context(state)
+
+    @decorate_with_context(
+        dummy_context_factory, idempotify_decorated_context=idempotify
+    )
+    def sample_function_idempotent():
+        assert state["context_active"] == True
+
+        # Nested invocation
+        @decorate_with_context(
+            dummy_context_factory, idempotify_decorated_context=idempotify
+        )
+        def nested_function():
+            assert state["context_active"] == True
+
+        nested_function()
+
+    sample_function_idempotent()
+    assert state["context_active"] == False
+    assert state["invocation_count"] == 1 + (not idempotify)
