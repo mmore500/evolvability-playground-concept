a2e78abaf03836abc9455fc3abd89f83fd41dbe0
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-10-03 12:20:58 -0400

    Patch up side effects on regimen tests

--- pylib/test/test_microsoro/test_simulation/test_get_default_update_regimen.py
+++ pylib/test/test_microsoro/test_simulation/test_get_default_update_regimen.py
@@ -1,56 +1,78 @@
+import copy
+
 from pylib.microsoro import (
     get_default_update_regimen,
     State,
     Structure,
     Params,
 )
-from pylib.microsoro.conditioners import ApplySpin
+from pylib.microsoro.conditioners import ApplySpin, ApplyTranslate
 
 
 def test_get_default_update_regimen():
     state1 = State()
+    ApplyTranslate(dpy=-2)(state1)
+    prestate1 = copy.deepcopy(state1)
+
     regimen1 = get_default_update_regimen()
     for step in regimen1:
         step(state1)
-    assert not State.same_position_as(state1, State())
+    assert not State.same_position_as(state1, prestate1)
+    assert not State.same_velocity_as(state1, prestate1)
 
     state2 = State()
+    ApplyTranslate(dpy=-2)(state2)
     ApplySpin()(state2)
+    prestate2 = copy.deepcopy(state2)
+
     regimen2 = get_default_update_regimen()
     for step in regimen2:
         step(state2)
-    assert not State.same_position_as(state1, state2)
+    assert not State.same_position_as(state2, prestate2)
+    assert not State.same_velocity_as(state2, prestate2)
 
-    state3 = State()
-    params3 = Params(g=100.0)
-    ApplySpin()(state3)
-    regimen3 = get_default_update_regimen(params3)
-    for step in regimen3:
-        step(state3)
-    assert not State.same_position_as(state1, state3)
+    assert not State.same_position_as(state1, state2)
+    assert not State.same_velocity_as(state1, state2)
+
+
+def test_get_default_update_regimen_params():
+
+    state1 = State()
+    ApplyTranslate(dpy=-2)(state1)
+    regimen1 = get_default_update_regimen()
+    for _update in range(100):  # need several updates to get past np.isclose
+        for step in regimen1:
+            step(state1)
+
+    state2 = State()
+    params2 = Params(g=100.0)
+    ApplyTranslate(dpy=-2)(state2)
+    regimen2 = get_default_update_regimen(params=params2)
+    for _update in range(100):  # need several updates to get past np.isclose
+        for step in regimen2:
+            step(state2)
+
+    assert not State.same_position_as(state1, state2)
+    assert not State.same_velocity_as(state1, state2)
 
 
 def test_get_default_update_regimen_structure():
     state1 = State()
+    ApplyTranslate(dpy=-2)(state1)
+    ApplySpin()(state1)
     regimen1 = get_default_update_regimen()
     for step in regimen1:
         step(state1)
-    assert not State.same_position_as(state1, State())
 
     state2 = State()
+    ApplyTranslate(dpy=-2)(state1)
     ApplySpin()(state2)
-    regimen2 = get_default_update_regimen()
+    params2 = Params(m=10.0)
+    regimen2 = get_default_update_regimen(
+        structure=Structure(params=params2),
+    )
     for step in regimen2:
         step(state2)
-    assert not State.same_position_as(state1, state2)
 
-    state3 = State()
-    params3 = Params(m=10.0)
-    ApplySpin()(state3)
-    regimen3 = get_default_update_regimen(
-        params=params3,
-        structure=Structure(params=params3),
-    )
-    for step in regimen3:
-        step(state3)
-    assert not State.same_position_as(state1, state3)
+    assert not State.same_position_as(state1, state2)
+    assert not State.same_velocity_as(state1, state2)
